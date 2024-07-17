824adf88a771ef1bc37acc5bf866bddefc34d840
kapitanluffy <kapitanluffy@gmail.com>
2024-07-17 15:19:17 +0800

    Rework delete_folder command

--- delete_folder.py
+++ delete_folder.py
@@ -1,34 +1,56 @@
+from .folder_utils import get_subdirectories, has_subdirectories, remove_dupe
 import sublime
 import sublime_plugin
 import os
 
-
 class FileManagerDeleteFolderCommand(sublime_plugin.WindowCommand):
+    def is_enabled(self):
+        return len(self.window.folders()) > 0
+
     def run(self):
         folders = self.window.folders()
-        self.window.show_quick_panel(folders, lambda index: self.on_root_pick(index, folders))
+        view = self.window.active_view()
+        file = view.file_name() if view is not None else None
 
-    def on_root_pick(self, index, folders):
-        if index < 0:
-            return
-        folder = folders[index]
+        if view is not None and file is not None and os.path.exists(file):
+            directory = os.path.dirname(file)
+            subdirectories = get_subdirectories(directory)
+            folders = subdirectories + folders
 
-        subdirectories = self.list_subdirectories(folder)
-        self.window.show_quick_panel(subdirectories, lambda index: self.on_done(index, subdirectories))
+        folders = remove_dupe(folders)
+        items = self.create_select_action_items(folders) + self.create_open_action_items(folders)
+        self.window.show_quick_panel(items, lambda index: self.on_done(index, items))
 
     def on_done(self, index, folders):
         if index < 0:
             return
         folder = folders[index]
 
-        self.view.window().run_command('delete_folder', {'dirs': [folder], 'prompt': True})
+        if folder.kind[2] == "Delete":
+            self.window.run_command('delete_folder', {'dirs': [folder.trigger[9:]], 'prompt': True})
 
-    # expensive as fck
-    def list_subdirectories(self, directory):
-        subdirectories = []
-        for item in os.listdir(directory):
-            item_path = os.path.join(directory, item)
-            if os.path.isdir(item_path):
-                subdirectories.append(item_path)
-                subdirectories.extend(self.list_subdirectories(item_path))
-        return subdirectories
+        if folder.kind[2] == "Open":
+            subdirectories = get_subdirectories(folder.trigger[9:])
+            items = self.create_select_action_items(subdirectories) + self.create_open_action_items(subdirectories)
+            self.window.show_quick_panel(items, lambda index: self.on_done(index, items))
+
+    def create_select_action_items(self, directories):
+        items = []
+        for directory in directories:
+            items.append(self.create_select_item(directory))
+        return items
+
+    def create_open_action_items(self, directories):
+        items = []
+        for directory in directories:
+            if has_subdirectories(directory):
+                items.append(self.create_open_item(directory))
+        return items
+
+    def create_select_item(self, value):
+        kind = (sublime.KindId.COLOR_REDISH, "D", "Delete")
+        return sublime.QuickPanelItem("[delete] {}".format(value), [], "", kind)
+
+    def create_open_item(self, value):
+        kind = (sublime.KindId.COLOR_GREENISH, "O", "Open")
+        return sublime.QuickPanelItem("  [open] {}".format(value), [], "", kind)
---
+++ folder_utils.py
@@ -0,0 +1,20 @@
+import os
+
+def has_subdirectories(directory):
+    for item in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, item)):
+            return True
+    return False
+
+def get_subdirectories(directory):
+    subdirectories = []
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path) is False:
+            continue
+        subdirectories.append(item_path)
+    return subdirectories
+
+def remove_dupe(items):
+    seen = {}
+    return [seen.setdefault(x, x) for x in items if x not in seen]
