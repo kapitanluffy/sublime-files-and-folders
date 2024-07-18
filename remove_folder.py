19cb9b04f6e03ec487933c2b15c985492a382782
kapitanluffy <kapitanluffy@gmail.com>
2024-07-18 14:15:56 +0800

    Rework remove_folder command

--- remove_folder.py
+++ remove_folder.py
@@ -1,14 +1,29 @@
+from .utils import KIND_REMOVE, get_subdirectories, has_subdirectories, remove_dupe
+import sublime
 import sublime_plugin
 
-
 class FileManagerRemoveFolderCommand(sublime_plugin.WindowCommand):
+    def is_enabled(self):
+        return len(self.window.folders()) > 0
+
     def run(self):
         folders = self.window.folders()
-        self.window.show_quick_panel(folders, lambda index: self.on_done(index, folders))
+        items = self.create_select_action_items(folders)
+        self.window.show_quick_panel(items, lambda index: self.on_done(index, items))
 
     def on_done(self, index, folders):
         if index < 0:
             return
         folder = folders[index]
 
-        self.window.run_command("remove_folder", {"dirs": [folder]})
+        if folder.kind[2] == KIND_REMOVE[2]:
+            self.window.run_command("remove_folder", {"dirs": [folder.trigger[9:]]})
+
+    def create_select_action_items(self, directories):
+        items = []
+        for directory in directories:
+            items.append(self.create_select_item(directory))
+        return items
+
+    def create_select_item(self, value):
+        return sublime.QuickPanelItem("{}".format(value), [], "", KIND_REMOVE)
