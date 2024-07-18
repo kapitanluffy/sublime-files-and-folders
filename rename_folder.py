3ee52b86f4efb4ea8e7c0dd4a2746e06273979f0
kapitanluffy <kapitanluffy@gmail.com>
2024-07-18 13:40:50 +0800

    Rework rename_folder command

--- rename_folder.py
+++ rename_folder.py
@@ -1,11 +1,56 @@
+from .folder_utils import get_subdirectories, has_subdirectories, remove_dupe
 import sublime
 import sublime_plugin
+import os
 
-from os import path
+class FileManagerRenameFolderCommand(sublime_plugin.WindowCommand):
+    def is_enabled(self):
+        return len(self.window.folders()) > 0
 
-class FileManagerRenameFolderCommand(sublime_plugin.TextCommand):
-    def run(self, edit, **kwargs):
-        directory = path.dirname(self.view.file_name())
-        self.view.window().run_command('rename_path', { 'paths': [directory] })
+    def run(self):
+        folders = self.window.folders()
+        view = self.window.active_view()
+        file = view.file_name() if view is not None else None
 
+        if view is not None and file is not None and os.path.exists(file):
+            directory = os.path.dirname(file)
+            subdirectories = get_subdirectories(directory)
+            folders = [directory] + subdirectories + folders
 
+        folders = remove_dupe(folders)
+        items = self.create_select_action_items(folders) + self.create_open_action_items(folders)
+        self.window.show_quick_panel(items, lambda index: self.on_done(index, items))
+
+    def on_done(self, index, folders):
+        if index < 0:
+            return
+        folder = folders[index]
+
+        if folder.kind[2] == "Rename":
+            self.window.run_command('rename_path', {'paths': [folder.trigger[9:]]})
+
+        if folder.kind[2] == "Open":
+            subdirectories = get_subdirectories(folder.trigger[9:])
+            items = self.create_select_action_items(subdirectories) + self.create_open_action_items(subdirectories)
+            self.window.show_quick_panel(items, lambda index: self.on_done(index, items))
+
+    def create_select_action_items(self, directories):
+        items = []
+        for directory in directories:
+            items.append(self.create_select_item(directory))
+        return items
+
+    def create_open_action_items(self, directories):
+        items = []
+        for directory in directories:
+            if has_subdirectories(directory):
+                items.append(self.create_open_item(directory))
+        return items
+
+    def create_select_item(self, value):
+        kind = (sublime.KindId.COLOR_GREENISH, "R", "Rename")
+        return sublime.QuickPanelItem("[delete] {}".format(value), [], "", kind)
+
+    def create_open_item(self, value):
+        kind = (sublime.KindId.COLOR_BLUISH, "O", "Open")
+        return sublime.QuickPanelItem("  [open] {}".format(value), [], "", kind)
