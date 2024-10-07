1cdbc54d5928416f1cfcbdc22bcfdd2b703491d7
kapitanluffy <485799+kapitanluffy@users.noreply.github.com>
2024-10-08 02:45:36 +0800

    Rework duplicate file command

--- new_file.py
+++ new_file.py
@@ -1,33 +1,67 @@
 import sublime
 import sublime_plugin
+from os import path, makedirs
 
-from os import path
+def get_target_dir(filename, basedirectory):
+    target_dir: str = path.dirname(filename)
+    extra_path = target_dir.replace(basedirectory, "").strip("/\\")
+    return path.abspath(path.join(basedirectory, extra_path))
+
+def get_view_name(view: sublime.View):
+    return view.name() or path.basename(view.file_name() or "untitled")
 
 class FileManagerInsertCommand(sublime_plugin.TextCommand):
     def run(self, edit, **kwargs):
         content = kwargs.get('content', False)
         syntax = kwargs.get('syntax', False)
-
-        if content != False:
+        if content is not False:
             self.view.insert(edit, 0, content)
-
-
-        if syntax != False:
+        if syntax is not False:
             self.view.assign_syntax(syntax)
 
+class FileManagerNewFileCommand(sublime_plugin.WindowCommand):
+    def run(self, **kwargs):
+        view = self.window.active_view()
+        if view is None:
+            return
 
-class FileManagerNewFileCommand(sublime_plugin.TextCommand):
-	def run(self, edit, **kwargs):
-		content = self.view.substr(sublime.Region(0, self.view.size()))
-		syntax = self.view.syntax()
+        is_dupe = kwargs.get('duplicate', False)
+        NEW_FILE_NAME = view.file_name() or ""
 
-		directory = path.dirname(self.view.file_name())
-		self.view.window().run_command('new_file_at', { 'dirs': [directory]})
+        directory = path.dirname(NEW_FILE_NAME) if NEW_FILE_NAME else path.expanduser('~')
+        # for now, fallback to the first directory
+        if directory is None and len(self.window.folders()) > 0:
+            directory = self.window.folders()[0]
 
-		if kwargs.get('duplicate') is True:
-			sublime.set_timeout_async(lambda: self.__duplicate(content, syntax), 100)
+        initial_text = path.join(directory or "", NEW_FILE_NAME)
 
-	def __duplicate(self, content, syntax):
-		view = self.view.window().active_view()
-		view.run_command('file_manager_insert', { "content": content, "syntax": syntax.path })
+        self.window.show_input_panel(
+            caption="New File:",
+            initial_text=initial_text,
+            on_done=lambda v: self.on_done(v, is_dupe, view, directory),
+            on_change=None,
+            on_cancel=None,
+        )
 
+    def on_done(self, filename: str, is_dupe, view, basedirectory):
+        filename = filename.strip() or get_view_name(view)
+        new_view = self.window.new_file()
+
+        if is_dupe is True:
+            content = view.substr(sublime.Region(0, view.size()))
+            syntax: sublime.Syntax = view.syntax()
+            new_view.assign_syntax(syntax)
+            new_view.run_command("file_manager_insert", {"content": content})
+
+        fullpath = path.join(
+            get_target_dir(filename, basedirectory),
+            path.basename(filename)
+        )
+        directory = path.dirname(fullpath)
+
+        if path.exists(directory) is False:
+            makedirs(directory)
+        if path.isdir(fullpath) is True:
+            new_view.settings().set('default_dir', fullpath)
+        if path.isdir(fullpath) is False:
+            new_view.retarget(fullpath)
